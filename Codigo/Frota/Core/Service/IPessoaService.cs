8056346f42045654d8c6ce3b8e428bf33b7d9545
Guilherme Lima Santos <grguilh2rm2lima@gmail.com>
2023-08-31 14:03:33 -0300

    Primeira implementação. Arquivos das classes IPessoaService.cs e PessoaService.cs.

---
+++ Codigo/Frota/Core/Service/IPessoaService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Service
+{
+    public interface IPessoaService
+    {
+        uint Creat(Pessoa pessoa);
+        void Edit(Pessoa pessoa);
+        void Delete(uint idPessoa);
+        IEnumerable<Pessoa> GetAll();
+    }
+}
---
+++ Codigo/Frota/Service/PessoaService.cs
@@ -0,0 +1,62 @@
+﻿using Core;
+using Core.Service;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    internal class PessoaService : IPessoaService
+    {
+        private readonly FrotaContext context;
+
+        public PessoaService(FrotaContext context)
+        {
+            this.context = context;
+        }
+        /// <summary>
+        /// Cadastra uma nova pessoa
+        /// </summary>
+        /// <param name="pessoa"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public uint Creat(Pessoa pessoa)
+        {
+            context.Add(pessoa);
+            context.SaveChanges();
+            return pessoa.Id;
+        }
+        /// <summary>
+        /// Exclui uma pessoa na base de dados
+        /// </summary>
+        /// <param name="idPessoa"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        public void Delete(uint idPessoa)
+        {
+            var pessoa = context.Pessoas.Find(idPessoa);
+            context.Remove(pessoa);
+            context.SaveChanges();
+        }
+        /// <summary>
+        /// Edita uma pessoa na base de dados
+        /// </summary>
+        /// <param name="pessoa"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        public void Edit(Pessoa pessoa)
+        {
+            context.Update(pessoa);
+            context.SaveChanges();
+        }
+        /// <summary>
+        /// Busca todas as pessoas cadastradas
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<Pessoa> GetAll()
+        {
+            return context.Pessoas.AsNoTracking();
+        }
+    }
+}
