64c8eb66ccb49494066973fe13657f087e2c14e7
Pedro Dantas <pedrood1232@gmail.com>
2023-09-01 10:58:15 -0300

    Adicionado controller, model e view para MarcaPecaInsumo #11

---
+++ Codigo/Frota/FrotaWeb/Controllers/MarcaPecaInsumoController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Service;
+
+namespace FrotaWeb.Controllers
+{
+    public class MarcaPecaInsumoController : Controller
+    {
+        private readonly IMarcaPecaInsumoService _marcaPecaInsumoService;
+
+        public MarcaPecaInsumoController(IMarcaPecaInsumoService marcaPecaInsumoService)
+        {
+            _marcaPecaInsumoService = marcaPecaInsumoService;
+        }
+
+        // GET: MarcaPecaInsumoController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: MarcaPecaInsumoController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: MarcaPecaInsumoController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MarcaPecaInsumoController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MarcaPecaInsumoController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: MarcaPecaInsumoController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MarcaPecaInsumoController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: MarcaPecaInsumoController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Codigo/Frota/FrotaWeb/Models/MarcaPecaInsumoViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FrotaWeb.Models
+{
+    public class MarcaPecaInsumoViewModel
+    {
+        [Display(Name = "Código")]
+        [Required]
+        public uint Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Descricao { get; set; } = null!;
+    }
+}
