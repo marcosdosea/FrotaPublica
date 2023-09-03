e09afc15ad4f26aff9d2f1986e28a6758960069e
victorgabrielvieiraa <88777214+victorgabrielvieiraa@users.noreply.github.com>
2023-09-03 11:37:22 -0300

    Inicialização da issue #3 adicionei IPecaInsumoService.cs PecainsumoViewModel.cs e PecaInsumoService.cs, ainda falta implementação em PecainsumoViewModel.cs.

---
+++ Codigo/Frota/Core/Service/IPecaInsumoService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Service
+{
+    public interface IPecaInsumoService
+    {
+        uint Create(Pecainsumo pecainsumo);
+
+        void Editar(Pecainsumo pecainsumo);
+
+       void Deletar(uint IdPeca);
+
+        Pecainsumo Obter (uint IdPeca);
+        
+        IEnumerable<Pecainsumo> GetAll();
+    }
+}
---
+++ Codigo/Frota/Service/PecaInsumoService.cs
@@ -0,0 +1,59 @@
+﻿using Core;
+using Core.Service;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    class PecaInsumoService : IPecaInsumoService
+    {
+        private readonly FrotaContext context;
+
+        public PecaInsumoService(FrotaContext context)
+        {
+            context = context;
+        }
+
+        public uint Create(Pecainsumo pecainsumo)
+        {
+            context.Add(pecainsumo);
+            context.SaveChanges();
+            return pecainsumo.Id;
+        }
+
+        public void Deletar(uint IdPeca)
+        {
+            var peca = context.Pecainsumos.Find(IdPeca);
+            context.Remove(peca);
+            context.SaveChanges();
+        }
+        public void Editar(Pecainsumo pecainsumo)
+        {
+            context.Update(pecainsumo);
+            context.SaveChanges();
+        }
+
+        public IEnumerable<Pecainsumo> GetAll()
+        {
+            return context.Pecainsumos.AsNoTracking(); 
+        }
+
+        public Pecainsumo Obter(uint IdPeca)
+        {
+            return context.Pecainsumos.Find(IdPeca);
+
+
+
+
+
+
+
+
+        }
+    }
+}
+   
