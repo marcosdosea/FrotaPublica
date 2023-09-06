09e62c0b5d840f3cc0aaf3fbc67fc05e5b038dc3
alcymar <alcymar16@gmail.com>
2023-09-06 11:28:33 -0300

    Adicionado regras de validação na classe ModeloVeiculoViewModel e finalização da implementação dessa classe. #2

--- Codigo/Frota/FrotaWeb/Models/ModeloVeiculoViewModel.cs
+++ Codigo/Frota/FrotaWeb/Models/ModeloVeiculoViewModel.cs
@@ -1,8 +1,28 @@
-﻿namespace FrotaWeb.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace FrotaWeb.Models
 {
     public class ModeloVeiculoViewModel
     {
 
+        [DisplayName("Código")]
+        [Required(ErrorMessage = "O Código do veiculo é necessario")]
+        [Key]
+        public int Id { get; set; }
+
+
+        [DisplayName("Código da Marca do Veiculo")]
+        [Required(ErrorMessage = "Código da marca é necessaria")]
+        public int IdMarca { get; set; }
+
+        [DisplayName("Modelo do Veiculo")]
+        [Required(ErrorMessage ="O modelo do veiculo é necessário")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "O nome do modelo deve ter o minimo de 2 caracteres")]
+        public string NomeModeloVeiculo { get; set; }
+
+        [DisplayName("Capacidade do tanque de combustivel")]
+        public int CapacidadeTanqueCombustivel { get; set; }
 
     }
 }
