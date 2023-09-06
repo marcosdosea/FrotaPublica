87e5a722893e4dc83afd32627db38b21901590bb
alcymar <alcymar16@gmail.com>
2023-09-06 11:49:07 -0300

    Iniciada a implementação dos metodos internos para funcionamento da classe ModeloVeiculoController.cs #2

--- Codigo/Frota/FrotaWeb/Controllers/ModeloVeiculoController.cs
+++ Codigo/Frota/FrotaWeb/Controllers/ModeloVeiculoController.cs
@@ -1,12 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Core.Service;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FrotaWeb.Controllers
 {
+
+
     public class ModeloVeiculoController : Controller
     {
+
+        private readonly IModeloVeiculoService modeloveiculoservice;
+
+        public ModeloVeiculoController(IModeloVeiculoService modeloveiculoservice)
+        {
+            this.modeloveiculoservice = modeloveiculoservice;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+
+
+
     }
 }
