1174508b6790cab2913f0d6a97378ce939923b8b
Jonatha Gabriel <jonathagabrielns@gmail.com>
2023-09-07 16:58:08 -0300

    Adicionado o controlador.

---
+++ Codigo/Frota/FrotaWeb/Controllers/SolicitacaomanutencaoController.cs
@@ -0,0 +1,106 @@
+using AutoMapper;
+using Core;
+using Core.Service;
+using FrotaWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FrotaWeb.Controllers
+{
+    public class SolicitacaomanutencaoController : Controller
+    {
+        private readonly ISolicitacaomanutencaoService _service;
+        private readonly IMapper _mapper;
+
+        public SolicitacaomanutencaoController(ISolicitacaomanutencaoService service, IMapper mapper)
+        {
+            _service = service;
+            _mapper = mapper;
+        }
+
+
+        // GET: SolicitacaomanutencaoController.cs
+        public ActionResult Index()
+        {
+            var listaSolicitacoes = _service.GetAll();
+            var listaSolicitacoesModel = _mapper.Map<List<SolicitacaomanutencaoViewModel>>(listaSolicitacoes);
+
+            return View(listaSolicitacoesModel);
+        }
+
+        // GET: SolicitacaomanutencaoController.cs/Details/5
+        public ActionResult Details(uint id)
+        {
+            var solicitacao = _service.Get(id);
+            var solicitacaoModel = _mapper.Map<SolicitacaomanutencaoViewModel>(solicitacao);
+
+            return View(solicitacaoModel);
+        }
+
+        // GET: SolicitacaomanutencaoController.cs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SolicitacaomanutencaoController.cs/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(SolicitacaomanutencaoViewModel solicitacaoModel)
+        {
+            if (ModelState.IsValid) {
+                var solicitacao = _mapper.Map<Solicitacaomanutencao>(solicitacaoModel);
+
+                _service.Create(solicitacao);
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: SolicitacaomanutencaoController.cs/Edit/5
+        public ActionResult Edit(uint id)
+        {
+            var solicitacao = _service.Get(id);
+            var solicitacaoModel = _mapper.Map<SolicitacaomanutencaoViewModel>(solicitacao);
+
+            return View(solicitacaoModel);
+        }
+
+        // POST: SolicitacaomanutencaoController.cs/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(uint id, SolicitacaomanutencaoViewModel solicitacaoModel)
+        {
+            if (ModelState.IsValid) {
+                var solicitacao = _mapper.Map<Solicitacaomanutencao>(solicitacaoModel);
+
+                _service.Edit(solicitacao);
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: SolicitacaomanutencaoController.cs/Delete/5
+        public ActionResult Delete(uint id)
+        {
+            return View();
+        }
+
+        // POST: SolicitacaomanutencaoController.cs/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(uint id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
+
