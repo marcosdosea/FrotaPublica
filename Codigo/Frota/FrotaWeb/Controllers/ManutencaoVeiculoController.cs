122c96d97f2d0a13f5a72eae82bcce00ea0fff01
Marcos Dósea <marcosdosea@yahoo.com.br>
2023-09-11 13:42:12 -0300

    correção de bug.

--- Codigo/Frota/FrotaWeb/Controllers/ManutencaoVeiculoController.cs
+++ Codigo/Frota/FrotaWeb/Controllers/ManutencaoVeiculoController.cs
@@ -1,14 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Core;
-using Core.Service;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Service
 {
-	public class ManutencaoVeiculoController : Controller
+    public class ManutencaoVeiculoController : Controller
 	{
 		private readonly IManutencaoService _manutencaoService;
 
