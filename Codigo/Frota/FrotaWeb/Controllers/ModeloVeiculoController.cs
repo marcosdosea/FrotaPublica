8fdee60ec5138609c9ae6f57b93f4bfe8511350d
alcymar <alcymar16@gmail.com>
2023-09-27 10:33:05 -0300

    Importação do AutoMapper e implementação do método Index da classe ModeloVeiculoController. #2

--- Codigo/Frota/FrotaWeb/Controllers/ModeloVeiculoController.cs
+++ Codigo/Frota/FrotaWeb/Controllers/ModeloVeiculoController.cs
@@ -1,5 +1,8 @@
-﻿using Core.Service;
+﻿using AutoMapper;
+using FrotaWeb.Models;
+using Core.Service;
 using Microsoft.AspNetCore.Mvc;
+using Service;
 
 namespace FrotaWeb.Controllers
 {
@@ -8,17 +11,26 @@ namespace FrotaWeb.Controllers
     public class ModeloVeiculoController : Controller
     {
 
-        private readonly IModeloVeiculoService modeloveiculoservice;
+        private readonly IModeloVeiculoService _modeloveiculoservice;
+        private readonly IMapper _mapper;
 
-        public ModeloVeiculoController(IModeloVeiculoService modeloveiculoservice)
+
+        /// <summary>
+        /// Utilização do mapper no construtor da classe
+        /// </summary>
+        /// <param name="modeloveiculoservice"></param>
+        /// <param name="mapper"></param>
+        public ModeloVeiculoController(IModeloVeiculoService modeloveiculoservice, IMapper mapper)
         {
-            this.modeloveiculoservice = modeloveiculoservice;
+            this._modeloveiculoservice = modeloveiculoservice;
+            this._mapper = mapper;
         }
 
         public IActionResult Index()
         {
-
-            return View();
+            var listaModelVeiculos = _modeloveiculoservice.GetAll();
+            var ModelVeiculos = _mapper.Map<List<ModeloVeiculoViewModel>>(listaModelVeiculos);
+            return View(ModelVeiculos);
         }
 
         public IActionResult Create()
