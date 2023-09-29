da195b16a2849854f92502256e7a7f43b1fdc5ca
alcymar <alcymar16@gmail.com>
2023-09-28 21:44:12 -0300

    Alteração do campo NomeVeiculo para Nome na classe ModeloVeiculoViewModel. #2

--- Codigo/Frota/FrotaWeb/Models/ModeloVeiculoViewModel.cs
+++ Codigo/Frota/FrotaWeb/Models/ModeloVeiculoViewModel.cs
@@ -19,7 +19,7 @@ namespace FrotaWeb.Models
         [DisplayName("Modelo do Veiculo")]
         [Required(ErrorMessage ="O modelo do veiculo é necessário")]
         [StringLength(50, MinimumLength = 2, ErrorMessage = "O nome do modelo deve ter o minimo de 2 caracteres")]
-        public string NomeModeloVeiculo { get; set; }
+        public string Nome { get; set; }
 
         [DisplayName("Capacidade do tanque de combustivel")]
         public int CapacidadeTanqueCombustivel { get; set; }
