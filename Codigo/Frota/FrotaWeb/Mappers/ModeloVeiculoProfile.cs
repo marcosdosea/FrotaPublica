a216b43718ce4d1ed758e29d3a339f0524970f9d
alcymar <alcymar16@gmail.com>
2023-09-28 21:36:59 -0300

    Implementação da classe ModeloVeiculoProfile utilizando o AutoMapper. #2

---
+++ Codigo/Frota/FrotaWeb/Mappers/ModeloVeiculoProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Core;
+using FrotaWeb.Models;
+
+namespace FrotaWeb.Mappers
+{
+    public class ModeloVeiculoProfile : Profile
+    {
+        public ModeloVeiculoProfile()
+        {
+            CreateMap<ModeloVeiculoViewModel, Modeloveiculo>().ReverseMap();
+        }
+    }
+}
