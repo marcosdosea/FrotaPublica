6582aec37f06bcea7ee4b6f5ea5fe6cf1937b253
alcymar <alcymar16@gmail.com>
2023-10-11 16:40:26 -0300

    Adicionada implementação do construtor, mappers e método Get da WebAPI na classe controller. #38

--- Codigo/Frota/FrotaApi/Controllers/PecaInsumoController.cs
+++ Codigo/Frota/FrotaApi/Controllers/PecaInsumoController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Core;
+using Core.Service;
+using FrotaWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -8,12 +12,25 @@ namespace FrotaApi.Controllers
     [ApiController]
     public class PecaInsumoController : ControllerBase
     {
-        // GET: api/<PecaInsumoController>
-        [HttpGet]
-        public IEnumerable<string> Get()
+
+        private readonly IPecaInsumoService _pecaInsumoService;
+        private readonly IMapper _mapper;
+
+        public PecaInsumoController(IPecaInsumoService pecaInsumoService, IMapper mapper)
         {
-            return new string[] { "value1", "value2" };
+            _pecaInsumoService = pecaInsumoService;
+            _mapper = mapper;
         }
+
+        [HttpGet]
+        // GET: api/<PecaInsumoController>
+        public ActionResult<List<PecaInsumoViewModel>> Get()
+        {
+            var listaPecasInsumos = _pecaInsumoService.GetAll();
+            var listaPecasInsumosModel = _mapper.Map<List<PecaInsumoViewModel>>(listaPecasInsumos);
+            return listaPecasInsumosModel;
+        }
+      
 
         // GET api/<PecaInsumoController>/5
         [HttpGet("{id}")]
