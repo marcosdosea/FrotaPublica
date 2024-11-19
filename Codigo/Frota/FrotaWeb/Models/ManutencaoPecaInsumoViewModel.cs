203bdf7577ed47e8cb71533ffaf47144274cb83d
Guilherme Lima Santos <grguilh2rm2lima@gmail.com>
2024-11-18 23:12:35 -0300

    Adicionada projeto service, interface e viewModel parcial. #120.

---
+++ Codigo/Frota/FrotaWeb/Models/ManutencaoPecaInsumoViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace FrotaWeb.Models
+{
+    public class ManutencaoPecaInsumoViewModel
+    {
+		[Key]
+		[DisplayName("Código")]
+		public uint IdManutencao { get; set; }
+
+		[Required(ErrorMessage = "O {0} é obrigatório")]
+		[DisplayName("Código da peça/insumo")]
+		public uint IdPecaInsumo { get; set; }
+
+		[Required(ErrorMessage = "O {0} é obrigatório")]
+		[DisplayName("Código da marca da peça/insumo")]
+		public uint IdMarcaPecaInsumo { get; set; }
+
+		[Required(ErrorMessage = "O {0} é obrigatório")]
+		public float Quantidade { get; set; }
+
+		[Required(ErrorMessage = "O {0} é obrigatório")]
+		[Range(0, 99999999999, ErrorMessage = "A {0} deve ser um número positivo com no máximo 11 dígitos")]
+		[DisplayName("Garantia (meses)")]
+		public int MesesGarantia { get; set; }
+
+		[Required(ErrorMessage = "O {0} é obrigatório")]
+		[Range(0, 99999999999, ErrorMessage = "A {0} deve ser um número positivo com no máximo 11 dígitos")]
+		[DisplayName("Garantia (Km)")]
+		public int KmGarantia { get; set; }
+
+		[Required(ErrorMessage = "O {0} é obrigatório")]
+		[DisplayName("Valor)")]
+		public decimal ValorIndividual { get; set; }
+
+		[Required(ErrorMessage = "O {0} é obrigatório")]
+		public decimal Subtotal { get; set; }
+	}
+}
---
+++ Codigo/Frota/Service/ManutencaoPecaInsumoService.cs
@@ -0,0 +1,71 @@
+﻿using Core;
+using Core.Service;
+using Microsoft.EntityFrameworkCore;
+
+namespace Service
+{
+	public class ManutencaoPecaInsumoService : IManutencaoPecaInsumoService
+	{
+		private readonly FrotaContext context;
+
+		public ManutencaoPecaInsumoService(FrotaContext context)
+		{
+			this.context = context;
+		}
+
+		/// <summary>
+		/// Adiciona nova manutencão de peça/insumo na base de dados
+		/// </summary>
+		/// <param name="manutencaoPecaInsumo"></param>
+		/// <returns></returns>
+		public uint Create(Manutencaopecainsumo manutencaoPecaInsumo)
+		{
+			context.Add(manutencaoPecaInsumo);
+			context.SaveChanges();
+			return manutencaoPecaInsumo.IdManutencao;
+		}
+
+		/// <summary>
+		/// Exclui uma manutencão de peça/insumo da base de dados
+		/// </summary>
+		/// <param name="id"></param>
+		public void Delete(uint id)
+		{
+			var manutencaoPecaInsumo = context.Manutencaopecainsumos.Find(id);
+			if (manutencaoPecaInsumo != null)
+			{
+				context.Remove(manutencaoPecaInsumo);
+				context.SaveChanges();
+			}
+		}
+
+		/// <summary>
+		/// Altera os dados de uma manutencão de peça/insumo na base de dados
+		/// </summary>
+		/// <param name="manutencaoPecaInsumo"></param>
+		public void Edit(Manutencaopecainsumo manutencaoPecaInsumo)
+		{
+			context.Update(manutencaoPecaInsumo);
+			context.SaveChanges();
+		}
+
+		/// <summary>
+		/// Obter uma manutencão de peça/insumo pelo id
+		/// </summary>
+		/// <param name="id"></param>
+		/// <returns></returns>
+		public Manutencaopecainsumo? Get(uint id)
+		{
+			return context.Manutencaopecainsumos.Find(id);
+		}
+
+		/// <summary>
+		/// Obter a lista de manutencões de peças/insumos cadastradas
+		/// </summary>
+		/// <returns></returns>
+		public IEnumerable<Manutencaopecainsumo> GetAll()
+		{
+			return context.Manutencaopecainsumos.AsNoTracking();
+		}
+	}
+}
