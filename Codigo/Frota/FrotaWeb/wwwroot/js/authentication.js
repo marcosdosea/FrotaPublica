fad5dcbf0bdf3a7257e73a9d747db535348f0cba
Guilherme Lima Santos <grguilh2rm2lima@gmail.com>
2024-11-19 05:56:39 -0300

    Finalizada atualizações nas views de login e registro, #130.

---
+++ Codigo/Frota/FrotaWeb/wwwroot/js/authentication.js
@@ -0,0 +1,14 @@
+﻿const togglePassword = document.querySelector("#eye-icon");
+const togglePasswordTwo = document.querySelector("#eye-icon-2"); // Para confirmação da senha da página de registro
+const password = document.querySelector("#password");
+const confirmPassword = document.querySelector("#confirm-password"); // Para confirmação da senha da página de registro
+function alterarVisibilidadeDoCampoSenha(icon, input) {
+    icon.addEventListener("click", function () {
+        const type = input.type === "password" ? "text" : "password";
+        input.type = type;
+        this.classList.toggle("fa-eye");
+        this.classList.toggle("fa-eye-slash");
+    });
+}
+alterarVisibilidadeDoCampoSenha(togglePassword, password);
+alterarVisibilidadeDoCampoSenha(togglePasswordTwo, confirmPassword);
