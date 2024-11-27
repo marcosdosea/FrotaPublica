4e5327c940e5bbdefe2d023ef665fde83ca5c5fd
K4U4dev <okaua057.cod@gmail.com>
2024-11-26 23:00:26 -0300

    Implementado o teste de integração, também foi realizada uma correção na lógica da autenticação de usuário, close #142

--- Codigo/Frota/FrotaWeb/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Codigo/Frota/FrotaWeb/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -18,126 +18,131 @@ using Microsoft.Extensions.Logging;
 
 namespace FrotaWeb.Areas.Identity.Pages.Account
 {
-	public class LoginModel : PageModel
-	{
-		private readonly SignInManager<UsuarioIdentity> _signInManager;
-		private readonly ILogger<LoginModel> _logger;
+    public class LoginModel : PageModel
+    {
+        private readonly SignInManager<UsuarioIdentity> _signInManager;
+        private readonly UserManager<UsuarioIdentity> userManager;
+        private readonly ILogger<LoginModel> _logger;
 
-		public LoginModel(SignInManager<UsuarioIdentity> signInManager, ILogger<LoginModel> logger)
-		{
-			_signInManager = signInManager;
-			_logger = logger;
-		}
+        public LoginModel(SignInManager<UsuarioIdentity> signInManager, UserManager<UsuarioIdentity> userManager, ILogger<LoginModel> logger)
+        {
+            _signInManager = signInManager;
+            this.userManager = userManager;
+            _logger = logger;
+        }
 
-		/// <summary>
-		///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-		///     directly from your code. This API may change or be removed in future releases.
-		/// </summary>
-		[BindProperty]
-		public InputModel Input { get; set; }
 
-		/// <summary>
-		///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-		///     directly from your code. This API may change or be removed in future releases.
-		/// </summary>
-		public IList<AuthenticationScheme> ExternalLogins { get; set; }
 
-		/// <summary>
-		///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-		///     directly from your code. This API may change or be removed in future releases.
-		/// </summary>
-		public string ReturnUrl { get; set; }
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        [BindProperty]
+        public InputModel Input { get; set; }
 
-		/// <summary>
-		///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-		///     directly from your code. This API may change or be removed in future releases.
-		/// </summary>
-		[TempData]
-		public string ErrorMessage { get; set; }
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public IList<AuthenticationScheme> ExternalLogins { get; set; }
 
-		/// <summary>
-		///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-		///     directly from your code. This API may change or be removed in future releases.
-		/// </summary>
-		public class InputModel
-		{
-			/// <summary>
-			///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-			///     directly from your code. This API may change or be removed in future releases.
-			/// </summary>
-			[Required(ErrorMessage = "O email é obrigatório")]
-			[StringLength(256, ErrorMessage = "O email não pode ter mais de 256 caracteres.")]
-			[EmailAddress]
-			public string Email { get; set; }
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public string ReturnUrl { get; set; }
 
-			/// <summary>
-			///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-			///     directly from your code. This API may change or be removed in future releases.
-			/// </summary>
-			[Required(ErrorMessage = "A senha é obrigatória")]
-			[StringLength(20, ErrorMessage = "A senha deve ter entre 8 e 20 caracteres", MinimumLength = 8)]
-			[DataType(DataType.Password)]
-			public string Password { get; set; }
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        [TempData]
+        public string ErrorMessage { get; set; }
 
-			/// <summary>
-			///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-			///     directly from your code. This API may change or be removed in future releases.
-			/// </summary>
-			[Display(Name = "Lembrar de mim?")]
-			public bool RememberMe { get; set; }
-		}
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public class InputModel
+        {
+            /// <summary>
+            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+            ///     directly from your code. This API may change or be removed in future releases.
+            /// </summary>
+            [Required(ErrorMessage = "O email é obrigatório")]
+            [StringLength(256, ErrorMessage = "O email não pode ter mais de 256 caracteres.")]
+            [EmailAddress]
+            public string Email { get; set; }
 
-		public async Task OnGetAsync(string returnUrl = null)
-		{
-			if (!string.IsNullOrEmpty(ErrorMessage))
-			{
-				ModelState.AddModelError(string.Empty, ErrorMessage);
-			}
+            /// <summary>
+            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+            ///     directly from your code. This API may change or be removed in future releases.
+            /// </summary>
+            [Required(ErrorMessage = "A senha é obrigatória")]
+            [StringLength(20, ErrorMessage = "A senha deve ter entre 8 e 20 caracteres", MinimumLength = 8)]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
 
-			returnUrl ??= Url.Content("~/");
+            /// <summary>
+            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+            ///     directly from your code. This API may change or be removed in future releases.
+            /// </summary>
+            [Display(Name = "Lembrar de mim?")]
+            public bool RememberMe { get; set; }
+        }
 
-			// Clear the existing external cookie to ensure a clean login process
-			await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
+        public async Task OnGetAsync(string returnUrl = null)
+        {
+            if (!string.IsNullOrEmpty(ErrorMessage))
+            {
+                ModelState.AddModelError(string.Empty, ErrorMessage);
+            }
 
-			ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+            returnUrl ??= Url.Content("~/");
 
-			ReturnUrl = returnUrl;
-		}
+            // Clear the existing external cookie to ensure a clean login process
+            await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
 
-		public async Task<IActionResult> OnPostAsync(string returnUrl = null)
-		{
-			returnUrl ??= Url.Content("~/");
+            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
 
-			ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+            ReturnUrl = returnUrl;
+        }
 
-			if (ModelState.IsValid)
-			{
-				// This doesn't count login failures towards account lockout
-				// To enable password failures to trigger account lockout, set lockoutOnFailure: true
-				var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
-				if (result.Succeeded)
-				{
-					_logger.LogInformation("User logged in.");
-					return RedirectToPage("/Home/Index");
-				}
-				if (result.RequiresTwoFactor)
-				{
-					return RedirectToPage("./LoginWith2fa", new { ReturnUrl = returnUrl, RememberMe = Input.RememberMe });
-				}
-				if (result.IsLockedOut)
-				{
-					_logger.LogWarning("User account locked out.");
-					return RedirectToPage("./Lockout");
-				}
-				else
-				{
-					ModelState.AddModelError(string.Empty, "Login inválido");
-					return Page();
-				}
-			}
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            returnUrl ??= Url.Content("~/");
 
-			// If we got this far, something failed, redisplay form
-			return Page();
-		}
-	}
+            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+
+            if (ModelState.IsValid)
+            {
+                // This doesn't count login failures towards account lockout
+                // To enable password failures to trigger account lockout, set lockoutOnFailure: true
+                var user = userManager.Users.SingleOrDefault(u => u.Email.ToLower() == Input.Email.ToLower());
+                var result = await _signInManager.PasswordSignInAsync(user, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation("User logged in.");
+                    return LocalRedirect(returnUrl);
+                }
+                if (result.RequiresTwoFactor)
+                {
+                    return RedirectToPage("./LoginWith2fa", new { ReturnUrl = returnUrl, RememberMe = Input.RememberMe });
+                }
+                if (result.IsLockedOut)
+                {
+                    _logger.LogWarning("User account locked out.");
+                    return RedirectToPage("./Lockout");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Login inválido");
+                    return Page();
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            return Page();
+        }
+    }
 }
---
+++ Codigo/Frota/ServiceTests/VeiculoPecaInsumoServiceTests.cs
@@ -0,0 +1,136 @@
+﻿using Core;
+using Core.Service;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Tests
+{
+    [TestClass()]
+    public class VeiculoPecaInsumoServiceTests
+    {
+        private FrotaContext? context;
+        private IVeiculoPecaInsumoService? service;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            // Arrange
+            var builder = new DbContextOptionsBuilder<FrotaContext>();
+            builder.UseInMemoryDatabase("Frota");
+            var options = builder.Options;
+
+            context = new FrotaContext(options);
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            List<Veiculopecainsumo> veiculoPecaInsumoList = new List<Veiculopecainsumo>
+            {
+                new Veiculopecainsumo
+                {
+                    IdVeiculo = 1,
+                    IdPecaInsumo = 101,
+                    DataFinalGarantia = new DateTime(2025, 12, 31),
+                    KmFinalGarantia = 50000,
+                    DataProximaTroca = new DateTime(2024, 06, 15),
+                    KmProximaTroca = 30000
+                },
+                new Veiculopecainsumo
+                {
+                    IdVeiculo = 2,
+                    IdPecaInsumo = 102,
+                    DataFinalGarantia = new DateTime(2026, 01, 15),
+                    KmFinalGarantia = 60000,
+                    DataProximaTroca = new DateTime(2024, 09, 20),
+                    KmProximaTroca = 35000
+                },
+                new Veiculopecainsumo
+                {
+                    IdVeiculo = 3,
+                    IdPecaInsumo = 103,
+                    DataFinalGarantia = new DateTime(2027, 05, 10),
+                    KmFinalGarantia = 75000,
+                    DataProximaTroca = new DateTime(2025, 02, 28),
+                    KmProximaTroca = 45000
+                }
+            };
+            context.AddRange(veiculoPecaInsumoList);
+            context.SaveChanges();
+            service = new VeiculoPecaInsumoService(context);
+        }
+
+        [TestMethod()]
+        public void CreateTest()
+        {
+            // Act
+            service!.Create(
+                new Veiculopecainsumo
+                {
+                    IdVeiculo = 4,
+                    IdPecaInsumo = 101,
+                    DataFinalGarantia = new DateTime(2025, 12, 31),
+                    KmFinalGarantia = 50000,
+                    DataProximaTroca = new DateTime(2024, 06, 15),
+                    KmProximaTroca = 30000
+                }
+            );
+
+            // Assert
+            Assert.AreEqual(4, service.GetAll().Count());
+            var veiculoPecaInsumo = service.Get(4, 101);
+            Assert.AreEqual(50000, veiculoPecaInsumo!.KmFinalGarantia);
+            Assert.AreEqual(30000, veiculoPecaInsumo!.KmProximaTroca);
+        }
+
+        [TestMethod()]
+        public void DeleteTest()
+        {
+            // Act
+            var veiculoPecaInsumo = service.Get(2, 102);
+            service!.Delete(veiculoPecaInsumo);
+            // Assert
+            Assert.AreEqual(2, service.GetAll().Count());
+            var veiculoPecaInsumoDeletado = service.Get(2, 102);
+            Assert.AreEqual(null, veiculoPecaInsumoDeletado);
+        }
+
+        [TestMethod()]
+        public void EditTest()
+        {
+            // Act
+            var veiculoPecaInsumo = service!.Get(1, 101);
+            veiculoPecaInsumo!.KmFinalGarantia = 1;
+            veiculoPecaInsumo!.KmProximaTroca = 1;
+            service.Edit(veiculoPecaInsumo);
+
+            // Assert
+            Assert.AreEqual(1, veiculoPecaInsumo!.KmFinalGarantia);
+            Assert.AreEqual(1, veiculoPecaInsumo!.KmProximaTroca);
+        }
+
+        [TestMethod()]
+        public void GetTest()
+        {
+            var veiculoPecaInsumo = service!.Get(1, 101);
+            Assert.IsNotNull(veiculoPecaInsumo);
+            Assert.AreEqual(50000, veiculoPecaInsumo.KmFinalGarantia);
+            Assert.AreEqual(30000, veiculoPecaInsumo.KmProximaTroca);
+        }
+
+        [TestMethod()]
+        public void GetAllTest()
+        {
+            // Act
+            var listaVeiculosPecaInsumos = service!.GetAll();
+            // Assert
+            Assert.IsInstanceOfType(listaVeiculosPecaInsumos, typeof(IEnumerable<Veiculopecainsumo>));
+            Assert.IsNotNull(listaVeiculosPecaInsumos);
+            Assert.AreEqual(3, listaVeiculosPecaInsumos.Count());
+        }
+    }
+}
