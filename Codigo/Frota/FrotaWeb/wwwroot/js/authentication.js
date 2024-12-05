c3b236115d2b87ccb0bda827df691a2fd1260ecf
Guilherme Lima Santos <grguilh2rm2lima@gmail.com>
2024-12-04 23:56:47 -0300

    Adicionada mácara de cpf para autenticação e menu aprimorado, #154.

--- Codigo/Frota/FrotaWeb/wwwroot/js/authentication.js
+++ Codigo/Frota/FrotaWeb/wwwroot/js/authentication.js
@@ -1,14 +1,28 @@
-﻿const togglePassword = document.querySelector("#eye-icon");
-const togglePasswordTwo = document.querySelector("#eye-icon-2"); // Para confirmação da senha da página de registro
-const password = document.querySelector("#password");
-const confirmPassword = document.querySelector("#confirm-password"); // Para confirmação da senha da página de registro
+﻿const togglePassword = document.querySelector("#eye-icon")
+const togglePasswordTwo = document.querySelector("#eye-icon-2") // Para confirmação da senha da página de registro
+const password = document.querySelector("#password")
+const confirmPassword = document.querySelector("#confirm-password") // Para confirmação da senha da página de registro
 function alterarVisibilidadeDoCampoSenha(icon, input) {
     icon.addEventListener("click", function () {
-        const type = input.type === "password" ? "text" : "password";
-        input.type = type;
-        this.classList.toggle("fa-eye");
-        this.classList.toggle("fa-eye-slash");
-    });
+        const type = input.type === "password" ? "text" : "password"
+        input.type = type
+        this.classList.toggle("fa-eye")
+        this.classList.toggle("fa-eye-slash")
+    })
 }
-alterarVisibilidadeDoCampoSenha(togglePassword, password);
-alterarVisibilidadeDoCampoSenha(togglePasswordTwo, confirmPassword);
+
+alterarVisibilidadeDoCampoSenha(togglePassword, password)
+if (togglePasswordTwo) {
+    alterarVisibilidadeDoCampoSenha(togglePasswordTwo, confirmPassword)
+}
+
+const inputCpf = document.querySelector("#cpf-input") // Para máscara do cpf
+
+inputCpf.addEventListener('keypress', () => {
+    let inputLength = inputCpf.value.length
+    if (inputLength === 3 || inputLength === 7) {
+        inputCpf.value += '.'
+    } else if (inputLength === 11) {
+        inputCpf.value += '-'
+    }
+})
