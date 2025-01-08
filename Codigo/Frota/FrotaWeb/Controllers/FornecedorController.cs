f076eb1e8c807a1f0f005cd3448968f98cd9f5ed
Guilherme Lima Santos <grguilh2rm2lima@gmail.com>
2025-01-08 14:33:53 -0300

    refactor: mover método da controladora para classe serviço de fornecedor, #183.

--- Codigo/Frota/FrotaWeb/Controllers/FornecedorController.cs
+++ Codigo/Frota/FrotaWeb/Controllers/FornecedorController.cs
@@ -109,24 +109,5 @@ namespace FrotaWeb.Controllers
             fornecedorService.Delete(id);
             return RedirectToAction(nameof(Index));
         }
-
-        [HttpGet]
-        public async Task<IActionResult> ConsultaCnpj(string cnpj)
-        {
-            using (var client = new HttpClient())
-            {
-                client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0");
-                var response = await client.GetAsync($"https://www.receitaws.com.br/v1/cnpj/{cnpj}");
-                if (response.IsSuccessStatusCode)
-                {
-                    var content = await response.Content.ReadAsStringAsync();
-                    return Content(content, "application/json");
-                }
-                else
-                {
-                    return BadRequest("Erro ao consultar CNPJ.");
-                }
-            }
-        }
     }
 }
--- Codigo/Frota/Service/FornecedorService.cs
+++ Codigo/Frota/Service/FornecedorService.cs
@@ -1,11 +1,6 @@
 ﻿using Core;
 using Core.Service;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Service
 {
@@ -17,8 +12,9 @@ namespace Service
         {
             this.context = context;
         }
+
         /// <summary>
-        /// Adionar nova frota na base de dados
+        /// Adionar um novo fornecedor na base de dados
         /// </summary>
         /// <param name="frota"></param>
         /// <returns></returns>
@@ -31,7 +27,7 @@ namespace Service
         }
 
         /// <summary>
-        /// Excluir uma frota da base de dados
+        /// Excluir um fornecedor da base de dados
         /// </summary>
         /// <param name="idFrota"></param>
         public bool Delete(uint idFornecedor)
@@ -46,9 +42,8 @@ namespace Service
             return false;
         }
 
-
         /// <summary>
-        /// Alterar os dados da frota na base de dados
+        /// Alterar os dados do fornecedor na base de dados
         /// </summary>
         /// <param name="frota"></param>
         public void Edit(Fornecedor fornecedor, int idFrota)
@@ -59,16 +54,17 @@ namespace Service
         }
 
         /// <summary>
-        /// Obter pelo id da frota
+        /// Obter pelo id do fornecedor
         /// </summary>
         /// <param name="idFrota"></param>
         /// <returns></returns>
-        public Fornecedor Get(uint idFornecedor)
+        public Fornecedor? Get(uint idFornecedor)
         {
             return context.Fornecedors.Find(idFornecedor);
         }
+
         /// <summary>
-        /// Obter a lista de frota cadastradas
+        /// Obter a lista de fornecedores cadastradas
         /// </summary>
         /// <returns></returns>
         public IEnumerable<Fornecedor> GetAll(int idFrota)
@@ -76,6 +72,29 @@ namespace Service
             return context.Fornecedors
                           .Where(fornecedor => fornecedor.IdFrota == idFrota)
                           .AsNoTracking();
+        }
+
+        /// <summary>
+        /// Consultar um cnpj através da api da ReceitaWS
+        /// </summary>
+        /// <param name="cnpj">O CNPJ a ser consultado.</param>
+        /// <returns>Uma tupla com um booleano indicando se a resposta foi bem-sucedida e o conteúdo ou mensagem de erro</returns>
+        public async Task<(bool Success, string Data)> ConsultaCnpj(string cnpj)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0");
+                var response = await client.GetAsync($"https://www.receitaws.com.br/v1/cnpj/{cnpj}");
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    return (true, content);
+                }
+                else
+                {
+                    return (false, response.IsSuccessStatusCode.ToString());
+                }
+            }
         }
     }
 }
