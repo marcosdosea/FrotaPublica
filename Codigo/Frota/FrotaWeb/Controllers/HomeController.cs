635d6d3cbcc0794cdb046b55eeb3f35b75a0ce8a
K4U4dev <okaua057.cod@gmail.com>
2025-03-07 04:32:46 -0300

    Adicionada uma index básica

--- Codigo/Frota/FrotaWeb/Controllers/HomeController.cs
+++ Codigo/Frota/FrotaWeb/Controllers/HomeController.cs
@@ -2,33 +2,52 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using Core;
+using Core.Service;
+using System.Data;
 
-namespace FrotaWeb.Controllers
+namespace FrotaWeb.Controllers;
+
+[Authorize]
+public class HomeController : Controller
 {
-    [Authorize]
-    public class HomeController : Controller
+    private readonly ILogger<HomeController> logger;
+    private readonly UserManager<UsuarioIdentity> userManager; // Injete o UserManager
+    private readonly IPessoaService pessoaService;
+
+    public HomeController(ILogger<HomeController> logger, UserManager<UsuarioIdentity> userManager, IPessoaService pessoaService)
     {
-        private readonly ILogger<HomeController> _logger;
+        this.logger = logger;
+        this.userManager = userManager;
+        this.pessoaService = pessoaService;
+    }
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+    public async Task<IActionResult> Index()
+    {
+        var viewModel = new HomeViewModel();
+        var user = await userManager.GetUserAsync(User);
+        viewModel.NameUser = pessoaService.GetNomePessoa(pessoaService.GetPessoaIdUser());
+        var roles = await userManager.GetRolesAsync(user);
+        string userRole = roles.FirstOrDefault();
+        viewModel.UserType = userRole;
+        //inicializar uma lista de lembretes manualmente que é uma lista de strings
+        viewModel.Lembretes = new List<string> { "Ação de cadastro pendente.", "Verifique a nova política de privacidade"};
+        viewModel.CoutLembretes = viewModel.Lembretes.Count();
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+        return View(viewModel);
+    }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+    public IActionResult Privacy()
+    {
+        return View();
+    }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
